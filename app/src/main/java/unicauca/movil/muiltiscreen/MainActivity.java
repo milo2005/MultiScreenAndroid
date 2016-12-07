21fe87b47567c25f4d7ea8e343c5404b0f0b41c3
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-12-06 19:58:02 -0500

    Proyect finished

--- app/src/main/java/unicauca/movil/muiltiscreen/MainActivity.java
+++ app/src/main/java/unicauca/movil/muiltiscreen/MainActivity.java
@@ -1,13 +1,69 @@
 package unicauca.movil.muiltiscreen;
 
+import android.graphics.Color;
+import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import unicauca.movil.muiltiscreen.fragments.ColorFragment;
+import unicauca.movil.muiltiscreen.fragments.ListFragment;
+
+public class MainActivity extends AppCompatActivity implements ListFragment.OnColorSelectedListener{
+
+    FragmentManager manager;
+    boolean phone, land, secondScreen;
+
+    ColorFragment color;
+    ListFragment list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.a_main);
+
+        secondScreen = false;
+        manager = getSupportFragmentManager();
+        phone = getResources().getBoolean(R.bool.phone);
+        land = getResources().getBoolean(R.bool.land);
+
+        list = ListFragment.instance();
+        color = ColorFragment.instance(0);
+        putFragment(R.id.container1, list);
+
+
+        if(land && !phone)
+            putFragment(R.id.container2, color);
+
+
+    }
+
+    private void putFragment(@IdRes int idContainer, Fragment fragment){
+        FragmentTransaction ft = manager.beginTransaction();
+        ft.replace(idContainer, fragment);
+        ft.commit();
+    }
+
+    @Override
+    public void onColorSelected(int posColor) {
+
+        if(land && !phone){
+            color.setColor(posColor);
+        }else{
+            color = ColorFragment.instance(posColor);
+            putFragment(R.id.container1, color);
+            secondScreen = true;
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(secondScreen)
+            putFragment(R.id.container1, list);
+        else
+            super.onBackPressed();
     }
 }
--- app/src/main/java/unicauca/movil/muiltiscreen/fragments/ListFragment.java
+++ app/src/main/java/unicauca/movil/muiltiscreen/fragments/ListFragment.java
@@ -1,28 +1,57 @@
 package unicauca.movil.muiltiscreen.fragments;
 
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
+import unicauca.movil.muiltiscreen.BR;
 import unicauca.movil.muiltiscreen.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements AdapterView.OnItemClickListener
+{
+
+    public interface OnColorSelectedListener{
+        void onColorSelected(int posColor);
+    }
+
+    ViewDataBinding binding;
+    OnColorSelectedListener onColorSelectedListener;
 
     public static ListFragment instance(){
         return new ListFragment();
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(context instanceof OnColorSelectedListener)
+            onColorSelectedListener = (OnColorSelectedListener) context;
+        else
+            throw new RuntimeException(context.toString()
+                    +" debe implementar OnColorSelectedListener");
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list,container, false);
+        binding.setVariable(BR.onItemClick, this);
+        return binding.getRoot();
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        onColorSelectedListener.onColorSelected(i);
+    }
 }
